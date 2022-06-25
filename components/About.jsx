bfa79947c6c4a0a58ddf9e4dbf75b81aa76fd05d
Brishan King <brishan.king@gmail.com>
2022-06-25 12:58:08 -0700

    Built foundation for portfolio website - was on a role all morning

---
+++ components/About.jsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='w-full md:h-screen p-4 flex items-center py-16'>
+      <div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+        <div className='col-span-2 flex flex-col justify-center'>
+          <p className='uppercase text-xl tracking-widest text-[#dd876c]'>About</p>
+          <h2>Who I Am</h2>
+          <p className='py-2 text-gray-600'>// Not your usual developer</p>
+          <p className='py-2 text-gray-600'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p className='py-2 text-gray-600'>Check out some of my latest projects.</p>
+        </div>
+        <div className='w-full h-auto shadow-xl shadow-gray-400 rounded-xl p-4 flex justify-center items-center hover:scale-105 eas-in duration-300'>
+          <Image className='rounded-xl' src='/../public/assets/portfolio-crop-portrait.jpg' alt='Portrait picture of myself' width='660' height='1000'/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default About;
---
+++ components/Skills.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import SkillCard from './SkillCard';
+
+const Skills = () => {
+  return (
+    <div className='w-full lg:h-screen p-4'>
+      <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+        <p className='text-xl tracking-widest uppercase text-[#dd876c]'>Skills</p>
+        <h2 className='py-4'>What I can do</h2>
+        <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
+          <SkillCard name='HTML' imgSrc={'/../public/assets/skills/html.png'}/>
+          <SkillCard name='CSS' imgSrc={'/../public/assets/skills/css.png'}/>
+          <SkillCard name='JavaScript' imgSrc={'/../public/assets/skills/javascript.png'}/>
+          <SkillCard name='React' imgSrc={'/../public/assets/skills/react.png'}/>
+          <SkillCard name='Sass' imgSrc={'/../public/assets/skills/sass.png'}/>
+          <SkillCard name='Node' imgSrc={'/../public/assets/skills/node.png'}/>
+          <SkillCard name='Tailwind' imgSrc={'/../public/assets/skills/tailwind.png'}/>
+          <SkillCard name='Github' imgSrc={'/../public/assets/skills/github.png'}/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills
