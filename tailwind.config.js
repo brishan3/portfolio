7c807c625cfddc49cd713e8bcb8690028c60f951
Brishan King <brishan.king@gmail.com>
2022-08-02 15:29:26 -0700

    Fix opaque background behind mobile navbar from obstructing items even when closed

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      transitionProperty: {
+        'background': 'bg-color, background, background-color',
+      }
+    },
   },
   plugins: [],
 }
