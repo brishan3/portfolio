e058973050470fe65425dcb35f413dd1f2e514e9
Brishan King <brishan.king@gmail.com>
2022-08-06 13:36:16 -0700

    Modify hover state on ProjectItem to be more fluid and smooth

--- components/ProjectItem.jsx
+++ components/ProjectItem.jsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 
 const ProjectItem = ({title, background, projectUrl, techStack}) => {
   return (
-    <article className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-br from-[#dd876c] to-[#8e7670]">
+    <article className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group bg-[#00000000] hover:bg-[#cf7f66] duration-1000">
       <Image
-        className="rounded-xl group-hover:opacity-10"
+        className="rounded-xl group-hover:opacity-10 opacity-100 duration-300"
         src={background}
         alt="/"
       />
-      <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
+      <div className="opacity-0 group-hover:opacity-100 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] duration-500">
         <h3 className="text-2xl text-white tracking-wider text-center">
           {title}
         </h3>
