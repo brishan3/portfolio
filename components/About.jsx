0cc9a3b9a38ea55ca8b4b05faf49902725f2931f
Brishan King <brishan.king@gmail.com>
2022-08-29 12:35:39 -0700

    Updated About section text to re-word some sentences and fix spelling mistake

--- components/About.jsx
+++ components/About.jsx
@@ -11,13 +11,13 @@ const About = () => {
           <h2>Who I Am</h2>
           <p className='py-2 text-gray-600'>// Web Developer</p>
           <p className='py-2 text-gray-600'>
-            As a kid, the 2D/3D graphics that captivated my consciousness morphed from something chunky and sharp, into a realm of uncanny-likeness - fit with dynamic lighting, real-time physics, weather systems, etc...
+            As I grew up, the 2D/3D graphics that captivated my consciousness morphed from something chunky and pixelated, into a realm of almost uncanny-likeness - fitted with dynamic lighting, real-time physics, weather systems, and the list goes on.
           </p>
           <p className='py-2 text-gray-600'>
-            I became enamoured with the endless possibilities of programming coupled with the ever expanding limits of computing power that seemingly trickles down to each of us. This infatuation sparked my imagination like nothing else: it is why I leaped into three years of Computing Science at SFU and it is why I am here today, currently pursuing a career in Software/Web Development.
+            I became absolutely enamoured with the endless possibilities of programming coupled with the ever expanding potentials of computing power that trickles down to each of us. This infatuation sparked my imagination like nothing else: it's why I leapt into three years of Computing Science at SFU followed by a Web Development bootcamp at BrainStation, and it's why I am here today, pursuing a career in Software/Web Development.
           </p>
           <p className='py-2 text-gray-600'>
-            I always had a sneaking suspicion I would be drawn back into the world of 1&apos;s and 0&apos;s that make our tech tick. I am passionate about solving real world problems or just exercising creativity in a virtual landscape. I am always looking for new opportunities, please feel free to reach out and connect!
+            I always had a sneaking suspicion I would be drawn into the world of 1&apos;s and 0&apos;s that make our tech tick. I am passionate about solving real world problems and exercising creativity in a virtual landscape. I am always looking for new opportunities, please feel free to reach out and connect!
           </p>
           <p className='py-2 text-gray-600'>Also, feel free to check out some of my latest projects!</p>
         </div>
