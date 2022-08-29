6f78a5e30d52a92a290f1770311818c4e44182e4
Brishan King <brishan.king@gmail.com>
2022-08-29 13:11:42 -0700

    Fixed unescaped entities in About section to comply with es-lint config

--- components/About.jsx
+++ components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
             As I grew up, the 2D/3D graphics that captivated my consciousness morphed from something chunky and pixelated, into a realm of almost uncanny-likeness - fitted with dynamic lighting, real-time physics, weather systems, and the list goes on.
           </p>
           <p className='py-2 text-gray-600'>
-            I became absolutely enamoured with the endless possibilities of programming coupled with the ever expanding potentials of computing power that trickles down to each of us. This infatuation sparked my imagination like nothing else: it's why I leapt into three years of Computing Science at SFU followed by a Web Development bootcamp at BrainStation, and it's why I am here today, pursuing a career in Software/Web Development.
+            I became absolutely enamoured with the endless possibilities of programming coupled with the ever expanding potentials of computing power that trickles down to each of us. This infatuation sparked my imagination like nothing else: it&apos;s why I leapt into three years of Computing Science at SFU followed by a Web Development bootcamp at BrainStation, and it&apos;s why I am here today, pursuing a career in Software/Web Development.
           </p>
           <p className='py-2 text-gray-600'>
             I always had a sneaking suspicion I would be drawn into the world of 1&apos;s and 0&apos;s that make our tech tick. I am passionate about solving real world problems and exercising creativity in a virtual landscape. I am always looking for new opportunities, please feel free to reach out and connect!
